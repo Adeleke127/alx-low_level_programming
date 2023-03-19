4b5314e8f1f1fc649ea0c8128e33f053f69874a8
Adeleke127 <holuwagbemiga09@gmail.com>
2023-03-19 15:21:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints line
-* Return: 0 (success)
+*main - A program that prints line
+*Return: 0 (success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");`
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
